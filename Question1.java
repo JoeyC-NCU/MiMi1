b475fffbc14b2f92d9d41d4b459c3caa1f238a16
AM021351 <am021351@au.edu.tw>
2015-04-27 11:05:48 +0800

    B AM021351 楊智宇提交

--- Question1.java
+++ Question1.java
@@ -1,6 +1,7 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
 		System.out.println("Test");
-		System.out.println("A AM021953 張哲中提交");
+		System.out.println("B AM021351 楊智宇提交");
+		
 	}
 }
