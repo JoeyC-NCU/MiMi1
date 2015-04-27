7668be8cffbe5f657473ee1985b3b3de994799da
AM021323 <am021323@au.edu.tw>
2015-04-27 11:50:58 +0800

    C revert B

--- Question1.java
+++ Question1.java
@@ -1,7 +1,6 @@
 ﻿public class Question1{
 	public statis void main(String[] args){
 		System.out.println("Test");
-		System.out.println("B AM021351 楊智宇提交");
-		
+		System.out.println("A AM021953 張哲中提交");
 	}
 }
